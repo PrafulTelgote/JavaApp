867932e52d4cca5085f4ebaa7492c002c6f29e9a
PrafulTelgote <prafultelgote4@gmail.com>
2022-06-23 12:30:58 +0530

    MyFrist_Project

---
+++ firstProject/src/com/masai/Main.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name:- "+"Praful");
+        System.out.println("Mother ‘s Name :- "+"AnjuTai");
+        System.out.println("  Age  :- "+25);
+        System.out.println("Gender :- "+"Male");
+        System.out.println("Address :- "+"Pune");
+        System.out.println("Mobile No :- "+"1234567898");
+
+    }
+}
