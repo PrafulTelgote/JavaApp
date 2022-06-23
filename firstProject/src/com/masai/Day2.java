071ba1842ecf99565d9da6fb80a09bab1f340342
PrafulTelgote <prafultelgote4@gmail.com>
2022-06-23 12:49:08 +0530

    day2_Practice

---
+++ firstProject/src/com/masai/Day2.java
@@ -0,0 +1,16 @@
+package com.masai;
+
+public class Day2 {
+    public static void main(String[] args)
+    {
+        int a=0;
+        int b=15;
+        System.out.println("a "+a);
+        System.out.println("a++ "+a++);
+        System.out.println("a "+a);
+        System.out.println(++a);//12
+        System.out.println(a--);//12
+        System.out.println(--a);//10
+        System.out.println(a);//10
+    }
+}
