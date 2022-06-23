5c05a83eab8eba3bb99247804e1b26b794851628
PrafulTelgote <prafultelgote4@gmail.com>
2022-06-24 00:43:20 +0530

    SUMOFNATURALNUMBERS ass

---
+++ firstProject/src/com/masai/SUMOFNATURALNUMBERS.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class SUMOFNATURALNUMBERS {
+    public static void main(String[] args)
+    {
+//        The set of positive numbers starting from1 is known as Natural Numbers.
+//        The sum of the first N natural numbers is given by the formula [N * (N + 1)] / 2
+//        For example, given a number 5, the sum of the first 5 natural numbers will be[5 * (6)]/2 = 15
+
+        int num=5;
+        int sum=(num*(num+1))/2;
+        System.out.println(sum);
+    }
+}
