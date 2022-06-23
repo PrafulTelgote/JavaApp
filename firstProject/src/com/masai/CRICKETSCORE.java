03369c9e1d10077aa7179188c29bbcf6067700ef
PrafulTelgote <prafultelgote4@gmail.com>
2022-06-24 00:52:39 +0530

    CRICKETSCORE

---
+++ firstProject/src/com/masai/CRICKETSCORE.java
@@ -0,0 +1,15 @@
+package com.masai;
+
+public class CRICKETSCORE {
+    public static void main(String[] args)
+    {
+        int OneRun=0;
+        int TwoRun=0;
+        int ThreeRun=0;
+        int six=0;
+        int four=0;
+        int run=4;
+        
+
+    }
+}
