b38dc2c8178c98ab5dbe1eafd6cde373babeae9d
PrafulTelgote <prafultelgote4@gmail.com>
2022-06-26 14:14:53 +0530

    STUDENTOBJECTDETAILS

---
+++ firstProject/src/com/masai/STUDENTOBJECTDETAILS.java
@@ -0,0 +1,21 @@
+package com.masai;
+import java.util.*;
+
+public class STUDENTOBJECTDETAILS {
+
+
+    public static void main(String args[])
+    {
+        
+        public class Student
+        {
+            Scanner sc=new Scanner(System.in);
+            int roll=sc.nextInt();
+            String name=sc.next();
+            int marks= sc.nextInt();
+        System.out.println("Roll No is: "+roll);
+        System.out.println("Name  is: "+name);
+        System.out.println("Marks ois: "+marks);
+        }
+    }
+}
